5b658960ecd8147059e4bde6556dbc26b4698ab0
Bea Chatel <bchatelbell@gmail.com>
2025-10-06 11:57:31 +0100

    Updated file structure

---
+++ pitch/Code/midi.js
@@ -0,0 +1,26 @@
+function setup(){
+WebMidi.enable(function(err) { //check if WebMidi.js is enabled
+  if (err) {
+    console.log("WebMidi could not be enabled.", err);
+  } else {
+    console.log("WebMidi enabled!");
+  }
+});
+
+//name our visible MIDI input and output ports
+console.log("---");
+console.log("Inputs Ports: ");
+for (i = 0; i < WebMidi.inputs.length; i++) {
+  console.log(i + ": " + WebMidi.inputs[i].name);
+}
+
+console.log("---");
+console.log("Output Ports: ");
+for (i = 0; i < WebMidi.outputs.length; i++) {
+  console.log(i + ": " + WebMidi.outputs[i].name);
+}
+}
+
+function draw(){
+
+}
